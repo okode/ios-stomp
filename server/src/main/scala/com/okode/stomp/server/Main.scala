0d07cca27819b4a94f400a93af4feac0bc653113
Pedro Jorquera <pjorquera@okode.com>
2015-01-04 16:31:31 +0100

    Using Spring Boot for bootstraping Stomp server

--- server/src/main/scala/com/okode/stomp/server/Main.scala
+++ server/src/main/scala/com/okode/stomp/server/Main.scala
@@ -16,21 +16,14 @@
 
 package com.okode.stomp.server
 
-import org.fusesource.stomp.scomp.StompClient
-import org.apache.activemq.apollo.stomp.StompFrame
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
 
-class Server {
+@EnableAutoConfiguration
+@ComponentScan
+class MainConfiguration
 
-  def main(args: Array[String]) {
-    val client = new StompClient
-    client.connect("localhost", 61613, "admin", "password")
-    0 to 100000 foreach((i:Int) => {
-      val message = "Hello: " + i
-      println(message)
-      client.send(destination = "/topic/demo", text = message)
-      Thread sleep 10
-    })
-    client.disconnect()
-  }
-
+object Main extends App {
+  SpringApplication.run(classOf[MainConfiguration], args:_ *);
 }
